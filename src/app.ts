fe30f2081136b9fe14369964895c2e7f879b1521
afshinrahmati <afshinrahmati93@gmail.com>
2022-08-05 22:46:29 +0430

    step 2

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 import deserializeUser from "./middleware/deserializeUser";
 const app = express();
 
+
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(deserializeUser);
 export default app;
